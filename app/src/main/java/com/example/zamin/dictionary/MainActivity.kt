f211d14c6b11e062dc47e6a55ececf7eaee777ef
SardorSafarov <zomin000s@gmail.com>
2022-09-26 09:21:35 +0500

    woriking

--- app/src/main/java/com/example/zamin/dictionary/MainActivity.kt
+++ app/src/main/java/com/example/zamin/dictionary/MainActivity.kt
@@ -1,9 +1,20 @@
 package com.example.zamin.dictionary
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.AssetManager
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.d
+import androidx.appcompat.app.AppCompatActivity
 import com.example.zamin.dictionary.databinding.ActivityMainBinding
+import com.example.zamin.dictionary.need.D
+import org.apache.poi.hssf.usermodel.HSSFCell
+import org.apache.poi.hssf.usermodel.HSSFRow
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.poifs.filesystem.POIFSFileSystem
+import java.io.InputStream
+import java.lang.Exception
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,6 +24,53 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.txt.setOnClickListener {
             startActivity(Intent(this,Page1Activity::class.java))
+        }
+        readExcelFileFromAssets()
+    }
+
+
+
+    fun readExcelFileFromAssets() {
+        try {
+            val myInput: InputStream
+            val assetManager: AssetManager = getAssets()
+            myInput = assetManager.open("book.xlsx")
+            val myFileSystem = POIFSFileSystem(myInput)
+            val myWorkBook = HSSFWorkbook(myFileSystem)
+            val mySheet = myWorkBook.getSheetAt(0)
+             D(mySheet.toString())
+            val rowIter = mySheet.rowIterator()
+            var rowno = 0
+            while (rowIter.hasNext()) {
+                val myRow = rowIter.next() as HSSFRow
+                if (rowno != 0) {
+                    val cellIter = myRow.cellIterator()
+                    var colno = 0
+                    var ikkinch = ""
+                    var uchinch = ""
+                    var turtinch = ""
+                    var beshinchi = ""
+                    while (cellIter.hasNext()) {
+                        val myCell = cellIter.next() as HSSFCell
+                        when (colno) {
+                            1 ->
+                                ikkinch = myCell.toString()
+                            2 ->
+                                uchinch = myCell.toString()
+                            3 ->
+                                turtinch = myCell.toString()
+                            4 ->
+                                beshinchi = myCell.toString()
+                        }
+
+                        colno++
+                         Log.d("sardor", " Index :" + myCell.columnIndex + " -- " + myCell.toString())
+                    }
+                }
+                rowno++
+            }
+        } catch (e: Exception) {
+            // Log.e(TAG, "error $e")
         }
     }
 }
---
+++ app/src/main/java/com/example/zamin/dictionary/need/need.kt
@@ -0,0 +1,7 @@
+package com.example.zamin.dictionary.need
+
+import android.util.Log
+
+fun D(message:String){
+    Log.d("sardor","|------------>   $message   <-------------------|")
+}
